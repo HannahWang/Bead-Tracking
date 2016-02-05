c4721325fac9800ba0d15433c55b6da0ff6e2690
HannahWang <b00910023@ntu.edu.tw>
2016-02-05 16:20:30 +0800

    add files

---
+++ get_beadtnxyz_han.m
@@ -0,0 +1,10 @@
+fin = fopen('bead_pos.txt','r');
+tline = fgets(fin);
+bead_tnxyz = zeros(0,5);
+while ischar(tline)
+    element = str2num(tline);
+    bead_tnxyz = [bead_tnxyz; element(1:5)];
+    tline = fgets(fin);
+end
+
+
