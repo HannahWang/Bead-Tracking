ed731de255840b1bf76a61d80fbf7050b8f0670f
HannahWang <b00910023@ntu.edu.tw>
2016-02-25 17:11:51 +0800

    add figure title to imshowpair image

--- showAllBGfilter.m
+++ showAllBGfilter.m
@@ -5,7 +5,7 @@ nors1 = [linspace(2,2,7) linspace(2,3,4) nor1_10_40(2:end-1) linspace(2.2,2.2,43
 k12_50_60 = linspace(2.3,2.6,11);
 k1s2 = [linspace(1.8,1.8,7) linspace(2.2,2.2,18) linspace(2.2,3,9) linspace(3,3,6) linspace(3,2.3,6) linspace(2.3,2.3,4) k12_50_60(1:end-1) linspace(2.6,2,6) linspace(2,2,4) linspace(2,1.8,11) linspace(1.8,1.8,29) linspace(1.8,1.7,11) linspace(1.7,1.7,49)];
 nors2 = [linspace(0,0,7) linspace(0.3,0.3,18) linspace(0.3,0.4,9) linspace(0.4,0.4,26) linspace(0.4,0.5,11) linspace(0.5,0.5,99)];
-for t = 1:2
+for t = 1:1
    if t == 1
       k1s = k1s1;
       nors = nors1;
@@ -13,7 +13,9 @@ for t = 1:2
        k1s = k1s2;
        nors = nors2;
    end
-   for z = 0:169
+   for z = 0:168
+       
        BGfilter(t,z,k1s(z+1),nors(z+1));
+       
    end
 end
